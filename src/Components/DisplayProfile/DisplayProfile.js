dc64eff56ff31a8b39271df2a0df6238ee9f4afb
AbdMamun1999 <mamunrox@gmail.com>
2022-05-28 23:20:11 +0600

    added displayProfie component in myProfile page

---
+++ src/Components/DisplayProfile/DisplayProfile.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import Loading from '../Loading/Loading';
+
+const DisplayProfile = () => {
+    const [users, setUsers] = useState({})
+    const [user, loading] = useAuthState(auth)
+
+    useEffect(() => {
+        fetch(`https://agile-dawn-56972.herokuapp.com/users/${user?.email}`)
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [users,user])
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    return (
+        <div>
+            <div className='w-2/4 lg:mt-20 ml-5'>
+                <h5 className='text-xl mt-2'>Name: <span>{user?.displayName}</span></h5>
+                <h5 className='text-xl mt-2'>Email: <span>{user?.email}</span></h5>
+                <h5 className='text-xl mt-2'>Educational LeveL: <span>{users?.educationLevel}</span></h5>
+                <h5 className='text-xl mt-2'>Name of Educational Institute: <span>{users?.educationalInstitute}</span></h5>
+                <h5 className='text-xl mt-2'>Phone: <span>{users?.phone}</span></h5>
+                <h5 className='text-xl mt-2'>Location: {users?.location}</h5>
+                <h5 className='text-xl mt-2'>Linkdin  Link: {users.linkdinLink}</h5>
+            </div>
+
+        </div>
+    );
+};
+
+export default DisplayProfile;
--- src/Components/MyProfile/MyProfile.js
+++ src/Components/MyProfile/MyProfile.js
@@ -3,6 +3,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
+import DisplayProfile from '../DisplayProfile/DisplayProfile';
 import Loading from '../Loading/Loading';
 
 const MyProfile = () => {
@@ -37,109 +38,112 @@ const MyProfile = () => {
     }
 
     return (
-        <>
-            <h1 className='text-center font-bold mt-5'>Update Your Profile</h1>
-            <div className='w-4/6 mx-auto border-2 p-10 my-5 rounded-md'>
-                <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-1 gap-3'>
-                    <div className="form-control">
-                        <input
-                            type="text"
-                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
-                            value={user?.displayName} readOnly disabled />
-                    </div>
-                    <div className="form-control">
-                        <input
-                            type="email"
-                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
-                            value={user?.email} readOnly disabled />
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+            <DisplayProfile></DisplayProfile>
+            <div>
+                <h1 className='text-center font-bold mt-5'>Update Your Profile</h1>
+                <div className='w-4/6 mx-auto border-2 p-10 my-5 rounded-md'>
+                    <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-1 gap-3'>
+                        <div className="form-control">
+                            <input
+                                type="text"
+                                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
+                                value={user?.displayName} readOnly disabled />
+                        </div>
+                        <div className="form-control">
+                            <input
+                                type="email"
+                                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
+                                value={user?.email} readOnly disabled />
 
-                    </div>
-                    <div className="form-control">
-                        <input
-                            type="text"
-                            placeholder="Name of Educational Institution"
-                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
-                            {...register("educationalInstitute", {
-                                required: {
-                                    value: true,
-                                    message: 'Educational Instutition is Require'
-                                }
+                        </div>
+                        <div className="form-control">
+                            <input
+                                type="text"
+                                placeholder="Name of Educational Institution"
+                                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
+                                {...register("educationalInstitute", {
+                                    required: {
+                                        value: true,
+                                        message: 'Educational Instutition is Require'
+                                    }
 
-                            })} />
-                        <label className="label">
-                            {errors?.educationalInstitute?.type === 'required' && <span className="label-text-alt text-red-600">{errors?.educationalInstitute.message}</span>}
-                        </label>
-                    </div>
-                    <div className="form-control">
-                        <input
-                            type="text"
-                            placeholder="Education Level"
-                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
-                            {...register("educationLevel", {
-                                required: {
-                                    value: true,
-                                    message: 'Education Level is Require'
-                                }
+                                })} />
+                            <label className="label">
+                                {errors?.educationalInstitute?.type === 'required' && <span className="label-text-alt text-red-600">{errors?.educationalInstitute.message}</span>}
+                            </label>
+                        </div>
+                        <div className="form-control">
+                            <input
+                                type="text"
+                                placeholder="Education Level"
+                                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
+                                {...register("educationLevel", {
+                                    required: {
+                                        value: true,
+                                        message: 'Education Level is Require'
+                                    }
 
-                            })} />
-                        <label className="label">
-                            {errors?.educationLevel?.type === 'required' && <span className="label-text-alt text-red-600">{errors?.educationLevel.message}</span>}
-                        </label>
-                    </div>
-                    <div className="form-control">
-                        <input
-                            type="text"
-                            placeholder="Phone Number"
-                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
-                            {...register("phone", {
-                                required: {
-                                    value: true,
-                                    message: 'phone Number is Require'
-                                }
+                                })} />
+                            <label className="label">
+                                {errors?.educationLevel?.type === 'required' && <span className="label-text-alt text-red-600">{errors?.educationLevel.message}</span>}
+                            </label>
+                        </div>
+                        <div className="form-control">
+                            <input
+                                type="text"
+                                placeholder="Phone Number"
+                                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
+                                {...register("phone", {
+                                    required: {
+                                        value: true,
+                                        message: 'phone Number is Require'
+                                    }
 
-                            })} />
-                        <label className="label">
-                            {errors?.phone?.type === 'required' && <span className="label-text-alt text-red-600">{errors?.phone.message}</span>}
-                        </label>
-                    </div>
-                    <div className="form-control">
-                        <input
-                            type="text"
-                            placeholder="Your Location (Road/Area/City)"
-                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
-                            {...register("location", {
-                                required: {
-                                    value: true,
-                                    message: 'Location is Require'
-                                }
+                                })} />
+                            <label className="label">
+                                {errors?.phone?.type === 'required' && <span className="label-text-alt text-red-600">{errors?.phone.message}</span>}
+                            </label>
+                        </div>
+                        <div className="form-control">
+                            <input
+                                type="text"
+                                placeholder="Your Location (Road/Area/City)"
+                                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
+                                {...register("location", {
+                                    required: {
+                                        value: true,
+                                        message: 'Location is Require'
+                                    }
 
-                            })} />
-                        <label className="label">
-                            {errors?.location?.type === 'required' && <span className="label-text-alt text-red-600">{errors?.location.message}</span>}
-                        </label>
-                    </div>
-                    <div className="form-control">
-                        <input
-                            type="text"
-                            placeholder="Your Linkdin Profile Link"
-                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
-                            {...register("linkdinLink", {
-                                required: {
-                                    value: true,
-                                    message: 'Linkdin Profile Link is Require'
-                                }
+                                })} />
+                            <label className="label">
+                                {errors?.location?.type === 'required' && <span className="label-text-alt text-red-600">{errors?.location.message}</span>}
+                            </label>
+                        </div>
+                        <div className="form-control">
+                            <input
+                                type="text"
+                                placeholder="Your Linkdin Profile Link"
+                                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black-600 dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 focus:border-black-600 peer"
+                                {...register("linkdinLink", {
+                                    required: {
+                                        value: true,
+                                        message: 'Linkdin Profile Link is Require'
+                                    }
 
-                            })} />
-                        <label className="label">
-                            {errors?.linkdinLink?.type === 'required' && <span className="label-text-alt text-red-600">{errors?.linkdinLink.message}</span>}
-                        </label>
-                    </div>
-                    <div>
-                        <input type="submit" className="btn btn-primary  w-full max-w-xs text-white" value='Submit' />
-                    </div>
-                </form>
+                                })} />
+                            <label className="label">
+                                {errors?.linkdinLink?.type === 'required' && <span className="label-text-alt text-red-600">{errors?.linkdinLink.message}</span>}
+                            </label>
+                        </div>
+                        <div>
+                            <input type="submit" className="btn btn-primary  w-full max-w-xs text-white" value='Submit' />
+                        </div>
+                    </form>
+                </div>
             </div>
-        </>
+        </div>
     );
 };
 
