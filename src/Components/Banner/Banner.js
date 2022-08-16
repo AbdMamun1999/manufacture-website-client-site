0f7cc0a57b5fd40075b64441e4807d97e8b491bf
Abdullah Al Mamun <mamun12121999@gmail.com>
2022-08-16 09:21:09 +0600

    added carousel on banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,21 +1,41 @@
 import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import imgOne from '../../images/slider/10_cashback_on_bkash_big_banner.jpg'
+import imgTwo from '../../images/slider/best_of_tp-link_routers_big_banner.jpg'
+import imgThree from '../../images/slider/big_banner_5_.jpg'
+import imgFour from '../../images/slider/big_banner_8gb_1_.jpg'
+import imgFive from '../../images/slider/big_banner_design.jpg'
+import imgSix from '../../images/slider/get_your_gadget_set_copy_big_banner.jpg'
+import imgSeven from '../../images/slider/offer_that_can_change_your_living_big_banner.jpg'
+
 
 const Banner = () => {
     return (
-        <div
-            className="hero min-h-screen bg-fixed"
-            style={{
-                backgroundImage: 'url(https://i.ibb.co/bKGGBZB/26349171.webp)',
-                // backgroundAttachment:'fixed'
-            }}>
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello Viewers</h1>
-                    <p className="mb-5 text-black font-bold text-2xl">Grand Offer for you 30%</p>
-                    <button className="btn btn-primary">Get Started</button>
+        <div className='w-full lg:w-[80%] border-2 mx-auto'>
+            <Carousel autoPlay showThumbs={false} infiniteLoop>
+                <div>
+                    <img src={imgOne} />
                 </div>
-            </div>
+                <div>
+                    <img src={imgTwo} />
+                </div>
+                <div>
+                    <img src={imgThree} />
+                </div>
+                <div>
+                    <img src={imgFour} />
+                </div>
+                <div>
+                    <img src={imgFive} />
+                </div>
+                <div>
+                    <img src={imgSix} />
+                </div>
+                <div>
+                    <img src={imgSeven} />
+                </div>
+            </Carousel >
         </div>
     );
 };
